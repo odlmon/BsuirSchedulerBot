a247c2d452bc282a67bfcc99d86fea216dec0717
odlmon <dimaosipov.2000@mail.ru>
2020-05-09 13:51:02 +0300

    Refactor: optimize imports

--- src/main/java/command/StartCommand.java
+++ src/main/java/command/StartCommand.java
@@ -1,14 +1,11 @@
 package command;
 
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
-import org.telegram.telegrambots.extensions.bots.commandbot.commands.helpCommand.HelpCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
-import java.sql.*;
 
 public class StartCommand extends BotCommand {
 
